915edc8f773538ad5065f72bda82ee6bc019279a
CrispStrobe <cze+github@mailbox.org>
2026-09-26 21:01:10 +0000

    record xv6 cpu map state in probe

--- scripts/probe-xv6-stock.mjs
+++ scripts/probe-xv6-stock.mjs
@@ -45,5 +45,6 @@ const receipt = {
   lapic: {svr: machine._lapic[0xf0 / 4], timer: machine._lapic[0x320 / 4], initialCount: machine._lapic[0x380 / 4]},
   ioapic: {id: machine._ioapic[0], version: machine._ioapic[1], ideLow: machine._ioapic[0x10 + 14 * 2], ideHigh: machine._ioapic[0x10 + 14 * 2 + 1], idePending: machine._apicIrq[14]},
   cpu: {cs: machine.cpu.cs, eip: machine.cpu.eip, eflags: machine.cpu.eflags, cr0: machine.cpu.cr0, cr3: machine.cpu.cr3, cr4: machine.cpu.cr4},
+  guestCpu: {ncpu: machine._read(0x111784), cpu0Apicid: machine._read(0x1117a0)},
 };
 console.log(JSON.stringify(receipt, null, 2));
